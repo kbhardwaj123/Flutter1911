f055c6a13007932cb39077e90cb470b54f178d88
Kshitij bhardwaj <kbhardwaj@ec.iitr.ac.in>
2019-12-10 23:52:39 +0530

    MDC 102 Home

--- first_app/lib/home.dart
+++ first_app/lib/home.dart
@@ -4,6 +4,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu,semanticLabel: 'menu',),
+          onPressed: () {
+            print('Menu Button');
+          },
+        ),
+        title: Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search,semanticLabel: 'search',),
+            onPressed: () {
+              print('Search Button');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.tune,semanticLabel: 'filter',),
+            onPressed: () {
+              print('filter Button');
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Text('You did it!!'),
         ),
