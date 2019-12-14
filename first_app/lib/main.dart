57ffa5426ab891b75e5f6a17859c932af8aaaf93
Kshitij bhardwaj <kbhardwaj@ec.iitr.ac.in>
2019-12-14 19:36:52 +0530

    backview complete

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -390,6 +390,7 @@ import 'package:first_app/menu_dashboard_layout.dart';
 
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
--- first_app/lib/menu_dashboard_layout.dart
+++ first_app/lib/menu_dashboard_layout.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final Color backgroundColor = Color(0xFF4A4A58);
+// final Color backgroundColor = Color(0xFF4A4A58);
 
 class MenuDashboard extends StatefulWidget {
   MenuDashboard({Key key}) : super(key: key);
@@ -10,11 +10,16 @@ class MenuDashboard extends StatefulWidget {
 }
 
 class _MenuDashboardState extends State<MenuDashboard> {
-  bool _is_collapsed = true;
+  bool _is_collapsed = false;
+  final Duration duration = Duration(milliseconds: 300);
+  double screenWidth,screenHeight;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    screenHeight = size.height;
+    screenWidth = size.width;
     return Scaffold(
-      backgroundColor: backgroundColor,
+      // backgroundColor: backgroundColor,
       body: Stack(children: <Widget>[
         menu(context),
         dashboard(context),
@@ -23,69 +28,79 @@ class _MenuDashboardState extends State<MenuDashboard> {
   }
  
   Widget menu(context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        SizedBox(height: 7.0),
-        ListTile(
-          contentPadding: const EdgeInsets.all(16.0),
-          leading: Icon(Icons.dashboard),
-          title: Text('Dashboard'),
-        ),
-        SizedBox(height: 7.0),
-        ListTile(
-          contentPadding: const EdgeInsets.all(16.0),
-          leading: Icon(Icons.message),
-          title: Text('Messages'),
-        ),
-        SizedBox(height: 7.0),
-        ListTile(
-          contentPadding: const EdgeInsets.all(16.0),
-          leading: Icon(Icons.update),
-          title: Text('Utility'),
-        ),
-        SizedBox(height: 7.0),
-        ListTile(
-          contentPadding: const EdgeInsets.all(16.0),
-          leading: Icon(Icons.functions),
-          title: Text('Funds Transfer'),
-        ),
-        SizedBox(height: 7.0),
-        ListTile(
-          contentPadding: const EdgeInsets.all(16.0),
-          leading: Icon(Icons.branding_watermark),
-          title: Text('Branches'),
-        ),
-        SizedBox(height: 7.0),
-      ],
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: screenHeight*0.1),
+          ListTile(
+            contentPadding: const EdgeInsets.all(5.0),
+            leading: Icon(Icons.dashboard),
+            title: Text('Dashboard'),
+          ),
+          ListTile(
+            contentPadding: const EdgeInsets.all(5.0),
+            leading: Icon(Icons.message),
+            title: Text('Messages'),
+          ),
+          ListTile(
+            contentPadding: const EdgeInsets.all(5.0),
+            leading: Icon(Icons.update),
+            title: Text('Utility'),
+          ),
+          ListTile(
+            contentPadding: const EdgeInsets.all(5.0),
+            leading: Icon(Icons.functions),
+            title: Text('Funds Transfer'),
+          ),
+          ListTile(
+            contentPadding: const EdgeInsets.all(5.0),
+            leading: Icon(Icons.branding_watermark),
+            title: Text('Branches'),
+          ),
+        ],
+      )
     );
   }
 
   Widget dashboard(context) {
-    return Material(
-      elevation: 8.0,
-      color: backgroundColor,
-      child: Column(
-        children: <Widget>[
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
+    return Container(
+      child: AnimatedPositioned(
+        top: _is_collapsed ? 0 : 0.2*screenHeight,
+        bottom: _is_collapsed ? 0 : 0.2*screenHeight,
+        right: _is_collapsed ? 0 : -0.1*screenWidth,
+        left: _is_collapsed ? 0 : 0.5*screenWidth,
+        duration: duration,
+        child: Material(
+          animationDuration: duration,
+          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+          elevation: 8.0,
+          child: Column(
             children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {
-                  setState(() {
-                    _is_collapsed = !_is_collapsed;
-                  });
-                },
-              ),
-              Text('Test App'),
-              Icon(Icons.settings)
-            ],
+              Padding(
+                padding: _is_collapsed ?  EdgeInsets.all(20.0) : EdgeInsets.all(10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {
+                        setState(() {
+                          _is_collapsed = !_is_collapsed;
+                        });
+                      },
+                    ),
+                    Text('Test App'),
+                    Icon(Icons.settings)
+                  ],
+                )
+              )
+            ],         
           ),
-        ],         
-      ),
+        )
+      )
     );
   }
 
